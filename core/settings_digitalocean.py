2892cb24cfbbea62d6e9489f4431d9f5913a6df7
ajmvfr <ajmvfr@gmail.com>
2024-10-27 17:54:53 -0400

    more changes

--- core/settings_digitalocean.py
+++ core/settings_digitalocean.py
@@ -72,7 +72,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
-# SECURE_SSL_REDIRECT = False 
+# SECURE_SSL_REDIRECT = True 
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # SECURE_HSTS_SECONDS = 31536000 # 1 year
